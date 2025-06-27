7ba4ad3c50c8f3a72cc467fa326ddb26aee52609
REOCA-OFFICAL <REOCA-OFFICAL@outlook.com>
2025-06-27 15:57:27 +0800

    Update 7x9w.js

--- config/7x9w.js
+++ config/7x9w.js
@@ -3,7 +3,13 @@
     fetch('config/8y3v.json')
         .then(r => r.json())
         .then(d => {
-            window._0xabc.username = d.u.split('').reverse().join('');
-            window._0xabc.password = d.p.split('').reverse().join('');
+            window._0xabc.loaded = false;
+            window._0xabc.username = decryptData(d.u);
+            window._0xabc.password = decryptData(d.p);
+            window._0xabc.loaded = true;
         });
 })();
+
+function decryptData(data) {
+    return atob(data).split('').reverse().join('');
+}
